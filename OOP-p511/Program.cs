67e8bf1e2ece15782dc77fd22669f8050e43d7be
kelfimov85-stack <k.elfimov85@gmail.com>
2025-11-23 14:49:23 +0500

    Изучен get и set

--- OOP-p511/Program.cs
+++ OOP-p511/Program.cs
@@ -11,100 +11,14 @@ namespace OOP_p511
     {
         static void Main(string[] args)
         {
-            Student student = new Student("Рома", "Елфимов", 14);
+            User user = new User(25, "re");
+            user.Print(user.Age, user.Name);
+            Console.WriteLine();
 
-            Console.WriteLine(student.FullName);
+            Phone phone = new Phone(200, "IPhoone", "10 pro");
+            phone.Print(phone.Price, phone.Brand, phone.Model);
 
             Console.ReadLine();
-        }
-    }
-
-    public class Student
-    {
-        private string _firstName;
-        private string _lastName;
-        private int _age;
-
-        public string FirstName
-        {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                if (true)
-                {
-                    value = _firstName;
-                }
-                else
-                {
-                    Console.WriteLine("Ошибка 1");
-                }
-            }
-        }
-        public string LastName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                if (true)
-                {
-                    value = _lastName;
-                }
-                else
-                {
-                    Console.WriteLine("Ошибка 2");
-                }
-            }
-        }
-        public int Age
-        {
-            get
-            {
-                return _age;
-            }
-            set
-            {
-                if (value > 0 && value < 100)
-                {
-                    _age = value;
-                }
-                else
-                {
-                    Console.WriteLine("Ошибка 3");
-                }
-            }
-        }
-
-        public Student(string firstName, string lastName, int age)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
-        }
-
-        public string FullName
-        {
-            get
-            {
-                return FirstName + LastName;
-            }
-            set
-            {
-                if(true)
-                {
-                    _firstName = value;
-                    _lastName = value;
-                }
-                else
-                {
-                    Console.WriteLine($"Ошибка 4");
-                }
-            }
         }
     }
 }
---
+++ OOP-p511/User.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_p511
+{
+    internal class User
+    {
+        private string _name;
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string Name {  
+            get 
+            {
+                return _name; 
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public User(int age, string name)
+        {
+            _name = name;
+            Age = age;
+        }
+
+        public void Print(int age, string name)
+        {
+            Console.Write($"{name} ");
+            Console.Write(age);
+        }
+    }
+
+    internal class Phone
+    {
+        private int _price;
+        private string _model;
+        private string _brand;
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                value = _price;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                value = _model;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                value = _brand;
+            }
+        }
+
+        public Phone(int price, string model, string brand) 
+        {
+            _price = price;
+            _model = model;
+            _brand = brand;
+        }
+
+        public void Print(int price, string brand, string model)
+        {
+            Console.WriteLine($"{price}, {brand}, {model}");
+        }
+    }
+}
