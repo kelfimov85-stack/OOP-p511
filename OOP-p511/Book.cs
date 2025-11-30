6f03ae4135a1163e9ff17c3748c51bd53b65b14f
kelfimov85-stack <k.elfimov85@gmail.com>
2025-11-30 14:40:29 +0500

    Задание Book

---
+++ OOP-p511/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_p511
+{
+    internal class Book
+    {
+        public string Author {  get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string author, string name, string publisher, int year, int pages)
+        {
+            Author = author;
+            Name = name;
+            Publisher = publisher;
+            Year = year;
+            Pages = pages;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Author}, {Name}, {Publisher},{Year}, {Pages}");
+        }
+    }
+}
